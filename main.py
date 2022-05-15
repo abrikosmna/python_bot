723894546111becdad530623a9f3e8e8bad3ab52
D1ffic00lt <dm.filinov@gmail.com>
2022-05-15 13:47:02 +0700

    испарвлена структура кода, удалены некоторые переменные

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
 import sqlite3
-import numpy as np
-from openpyxl import load_workbook
 import httplib2
-import pandas as pd
 import vk_api
+import pandas as pd
+import smtplib
+
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll
+from openpyxl import load_workbook
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
-import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from platform import python_version
+
 import TOKEN_bot
 
 post_mail = [[]]
@@ -24,12 +25,12 @@ oformlenie = "Нажмите кнопку <сделать заказ> чтобы
 city_user = ""
 product_in_stock_wb = pd.read_excel("product_in_stock.xlsx", sheet_name="Лист1")
 true_city = 0
-
-
+recipient = None
+product_in_stock = None
+not_in_stock = None
 vk_session = vk_api.VkApi(token=TOKEN_bot.TOKEN)
 session_api = vk_session.get_api()
 longpool = VkLongPoll(vk_session)
-
 
 db = sqlite3.connect("info.db")
 sql = db.cursor()
@@ -77,56 +78,46 @@ def Fix_msg(msg):
     return msg
 
 
-def product_stock(id):
+def product_stock(ID: int):
     global product_in_stock, not_in_stock
     not_in_stock = 0
-    money = 0
+    money_ = 0
     for value in sql.execute("SELECT * FROM users"):
         product_in_stock = value[6]
     colum_a = sheet_xlsl["A"]
     for i in range(1, len(colum_a)):
         if int(colum_a[i].value) == int(product_in_stock):
-            money = sheet_xlsl['C' + str(i + 1)].value
+            money_ = sheet_xlsl['C' + str(i + 1)].value
             if sheet_xlsl['B' + str(i + 1)].value == 0:
-                send_message(id, "Извините, но этот товар закончился. Выбирите другой")
+                send_message(ID, "Извините, но этот товар закончился. Выверите другой")
                 not_in_stock = 1
             else:
-                send_message(id, "Введите ваш город")
-    print(money)
-    return money, not_in_stock
+                send_message(ID, "Введите ваш город")
+    print(money_)
+    return money_, not_in_stock
 
 
-def post_mail_func(post_mail, id):
+def post_mail_func(email, ID: int):
     global city_user, true_city
     for value in sql.execute("SELECT * FROM users"):
         city_user = value[7].capitalize()
-    send_message(id, "Одну минтку, ищем для вас пункты выдачи")
+    send_message(ID, "Одну минтку, ищем для вас пункты выдачи")
     for i, row in wb.iterrows():
         if row['Город'] == city_user:
             true_city = 1
-            post_mail[0].append(wb['Адрес'][i])
+            email[0].append(wb['Адрес'][i])
     if true_city == 0:
-        send_message(id, "Извините, но мы не осуществляем доставку в ваш город")
-    post_mail = np.asarray(post_mail).reshape(-1, 1)
-    post_mail_colum = post_mail.tolist()
+        send_message(ID, "Извините, но мы не осуществляем доставку в ваш город")
     if true_city == 1:
-        rangeAll = '{0}!A1:ZZ'.format("Лист1")
-        body = {}
-        resultClear = sheet.values().clear(
-            spreadsheetId=sheet_id,
-            range=rangeAll,
-            body=body).execute()
-        resp = sheet.values().update(
-            spreadsheetId=sheet_id,
-            range='лист1!A1',
-            valueInputOption="RAW",
-            body={'values': post_mail_colum}).execute()
-        send_message(id,
-                 "https://docs.google.com/spreadsheets/d/1PbxNtvA6Kt6F3-IoRhBscrNNRmHaAyg8jiFnhXr-yPM/edit?usp=sharing")
-        send_message(id, "Выберите пункт выдачи из предложенного списка")
+        send_message(
+            ID,
+            "https://docs.google.com/spreadsheets/d/1PbxNtvA6Kt6F3-IoRhBscrNNRmHaAyg8jiFnhXr-yPM/edit?usp=sharing"
+        )
+        send_message(ID, "Выберите пункт выдачи из предложенного списка")
     return true_city
 
-def send_email(money):
+
+def send_email(money_):
     global recipient
     for value in sql.execute("SELECT * FROM users"):
         recipient = value[5].capitalize()
@@ -137,7 +128,7 @@ def send_email(money):
     sender = user
     subject = 'Заказ принят'
     text = f'Спасибо за заказ <br>' \
-           f' сумма заказа {money} <br>' \
+           f' сумма заказа {money_} <br>' \
            f' Ждем вас еще в нашем магазине'
     html = '<html><head></head><body><p>' + text + '</p></body></html>'
     msg = MIMEMultipart('alternative')
@@ -158,80 +149,81 @@ def send_email(money):
 
 
 def main():
-    global city_user, resp, true_city, post_mail, product_in_stock, money, not_in_stock, recipient
+    global city_user, true_city, post_mail, product_in_stock, money, not_in_stock, recipient
     for event in longpool.listen():
         if event.type == vk_api.longpoll.VkEventType.MESSAGE_NEW and event.to_me:
             msg = event.text.lower()
-            id = event.user_id
-            sql.execute(f" SELECT userId FROM users WHERE userId = '{id}'")
+            ID = event.user_id
+            sql.execute(f" SELECT userId FROM users WHERE userId = '{ID}'")
             if sql.fetchone() is None:
                 sql.execute("INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?)",
-                            (id, "newUser", "0", "0", "0", "0", "0", "0", "0"))
+                            (ID, "newUser", "0", "0", "0", "0", "0", "0", "0"))
                 db.commit()
-                send_message(id, "Привет для начало оформления заказа напиши старт")
+                send_message(ID, "Привет для начало оформления заказа напиши старт")
             else:
-                user_act = sql.execute(f"SELECT act FROM users WHERE userId ='{id}'").fetchone()[0]
-                if user_act == "newUser" and msg == "старт":
+                user_action = sql.execute(f"SELECT act FROM users WHERE userId ='{ID}'").fetchone()[0]
+                if user_action == "newUser" and msg == "старт":
                     keyboard = VkKeyboard(one_time=True)
                     keyboard.add_button("Сделать заказ", color=VkKeyboardColor.PRIMARY)
-                    send_message(id, oformlenie, keyboard)
+                    send_message(ID, oformlenie, keyboard)
 
-                elif user_act == "newUser" and msg == "сделать заказ":
-                    send_message(id, "Начало оформления заказа")
-                    send_message(id, "Напишите свое ФИО")
-                    sql.execute(f"UPDATE users SET act = 'Get_fio' WHERE userId = {id}")
+                elif user_action == "newUser" and msg == "сделать заказ":
+                    send_message(ID, "Начало оформления заказа")
+                    send_message(ID, "Напишите свое ФИО")
+                    sql.execute(f"UPDATE users SET act = 'Get_fio' WHERE userId = {ID}")
                     db.commit()
-                elif user_act == "Get_fio":
-                    sql.execute(f"UPDATE users SET fio ={Fix_msg(msg)} WHERE userId = {id}")
-                    sql.execute(f"UPDATE users SET act = 'Get_date_of_birth' WHERE userId = {id}")
+                elif user_action == "Get_fio":
+                    sql.execute(f"UPDATE users SET fio ={Fix_msg(msg)} WHERE userId = {ID}")
+                    sql.execute(f"UPDATE users SET act = 'Get_date_of_birth' WHERE userId = {ID}")
                     db.commit()
-                    send_message(id, "Введите вашу дату рождения")
-                elif user_act == "Get_date_of_birth":
-                    sql.execute(f"UPDATE users SET date_of_birth ={Fix_msg(msg)} WHERE userId = {id}")
-                    sql.execute(f"UPDATE users SET act = 'Get_telephone' WHERE userId = {id}")
+                    send_message(ID, "Введите вашу дату рождения")
+                elif user_action == "Get_date_of_birth":
+                    sql.execute(f"UPDATE users SET date_of_birth ={Fix_msg(msg)} WHERE userId = {ID}")
+                    sql.execute(f"UPDATE users SET act = 'Get_telephone' WHERE userId = {ID}")
                     db.commit()
-                    send_message(id, "Введите ваш номер телефона")
-                elif user_act == "Get_telephone":
-                    sql.execute(f"UPDATE users SET telephone ={Fix_msg(msg)} WHERE userId = {id}")
-                    sql.execute(f"UPDATE users SET act = 'Get_emal' WHERE userId = {id}")
+                    send_message(ID, "Введите ваш номер телефона")
+                elif user_action == "Get_telephone":
+                    sql.execute(f"UPDATE users SET telephone ={Fix_msg(msg)} WHERE userId = {ID}")
+                    sql.execute(f"UPDATE users SET act = 'Get_emal' WHERE userId = {ID}")
                     db.commit()
-                    send_message(id, "Введите вашу электронную почту")
-                elif user_act == "Get_emal":
-                    sql.execute(f"UPDATE users SET emal ={Fix_msg(msg)} WHERE userId = {id}")
-                    sql.execute(f"UPDATE users SET act = 'Get_pos_produc' WHERE userId = {id}")
+                    send_message(ID, "Введите вашу электронную почту")
+                elif user_action == "Get_emal":
+                    sql.execute(f"UPDATE users SET emal ={Fix_msg(msg)} WHERE userId = {ID}")
+                    sql.execute(f"UPDATE users SET act = 'Get_pos_produc' WHERE userId = {ID}")
                     db.commit()
-                    send_message(id,
-                                 "Введите позицию товара, который хотите заказть(позицию можно посмотреть на страничке сообщества)")
-                elif user_act == "Get_pos_produc":
-                    sql.execute(f"UPDATE users SET pos_produc ={Fix_msg(msg)} WHERE userId = {id}")
-                    sql.execute(f"UPDATE users SET act = 'Get_city' WHERE userId = {id}")
+                    send_message(
+                        ID,
+                        "Введите позицию товара, который хотите заказaть"
+                        "(позицию можно посмотреть на страничке сообщества)"
+                    )
+                elif user_action == "Get_pos_produc":
+                    sql.execute(f"UPDATE users SET pos_produc ={Fix_msg(msg)} WHERE userId = {ID}")
+                    sql.execute(f"UPDATE users SET act = 'Get_city' WHERE userId = {ID}")
                     db.commit()
-                    money, not_in_stock = product_stock(id)
-                elif user_act == "Get_city" and not_in_stock == 1:
-                    sql.execute(f"UPDATE users SET pos_produc ={Fix_msg(msg)} WHERE userId = {id}")
-                    sql.execute(f"UPDATE users SET act = 'Get_city' WHERE userId = {id}")
+                    money, not_in_stock = product_stock(ID)
+                elif user_action == "Get_city" and not_in_stock == 1:
+                    sql.execute(f"UPDATE users SET pos_produc ={Fix_msg(msg)} WHERE userId = {ID}")
+                    sql.execute(f"UPDATE users SET act = 'Get_city' WHERE userId = {ID}")
                     db.commit()
-                    money, not_in_stock = product_stock(id)
+                    money, not_in_stock = product_stock(ID)
                     not_in_stock = 0
-                elif user_act == "Get_city" and not_in_stock == 0:
-                    sql.execute(f"UPDATE users SET city ={Fix_msg(msg)} WHERE userId = {id}")
-                    sql.execute(f"UPDATE users SET act = 'Get_post' WHERE userId = {id}")
+                elif user_action == "Get_city" and not_in_stock == 0:
+                    sql.execute(f"UPDATE users SET city ={Fix_msg(msg)} WHERE userId = {ID}")
+                    sql.execute(f"UPDATE users SET act = 'Get_post' WHERE userId = {ID}")
                     db.commit()
-                    post_mail_func(post_mail, id)
-                elif user_act == "Get_post":
-                    sql.execute(f"UPDATE users SET post ={Fix_msg(msg)} WHERE userId ={id}")
-                    sql.execute(f"UPDATE users SET act = 'REG' WHERE userId ={id}")
+                    post_mail_func(post_mail, ID)
+                elif user_action == "Get_post":
+                    sql.execute(f"UPDATE users SET post ={Fix_msg(msg)} WHERE userId ={ID}")
+                    sql.execute(f"UPDATE users SET act = 'REG' WHERE userId ={ID}")
                     db.commit()
-                    send_message(id, f'Сумма: {money}\n Ссылка:')
-                    send_message(id, "Спасибо за заказ")
+                    send_message(ID, f'Сумма: {money}\n Ссылка:')
+                    send_message(ID, "Спасибо за заказ")
                     send_email(money)
-                    send_message(id, "Если хотите еще раз заказать напишите <заказ>")
-                elif user_act == "REG":
+                    send_message(ID, "Если хотите еще раз заказать напишите <заказ>")
+                elif user_action == "REG":
                     send_message(id, "Напишите номер позиции нового заказа")
-                    sql.execute(f"UPDATE users SET act = 'Get_pos_produc' WHERE userId = {id}")
+                    sql.execute(f"UPDATE users SET act = 'Get_pos_produc' WHERE userId = {ID}")
                     db.commit()
 
 
 main()
-
-
